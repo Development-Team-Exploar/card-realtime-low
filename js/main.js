fc0f92a7ab966476a1773769cd03358486798bff
Sourabh <devteam@liminal.in>
2022-07-13 19:18:29 +0530

    cube rare image & vids flipped

--- js/main.js
+++ js/main.js
@@ -356,11 +356,6 @@ engine.runRenderLoop(function () {
     scene.render();
 });
 
-// Watch for browser/canvas resize events
-window.addEventListener("resize", function () {
-    engine.resize();
-});
-
 function toggleContentOverlay(){
 
     leftBigImg.children[0].pause();
@@ -595,6 +590,7 @@ scene.onPointerMove = function(event){
 
 const checkObjectSizePositions = () => {
     checkMobile()
+    engine.resize();
     sizes.width = window.innerWidth;
     sizes.height = window.innerHeight;
 
